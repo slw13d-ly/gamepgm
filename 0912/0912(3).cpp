1ef2b818a9e1155311756f045386bfc31552da60
slw13d-ly <jinye24@naver.com>
2024-09-19 05:02:18 +0900

    Update 0912(3).cpp

--- 0912/0912(3).cpp
+++ 0912/0912(3).cpp
@@ -1,3 +1,26 @@
+//정사각형 표현
+#include <stdio.h>
+
+void draw_basic_square(void);
+int main(void)
+{
+	draw_basic_square();
+	return 0;
+}
+
+void draw_basic_square(void)
+{
+	unsigned char a=0xa6, b[7], i;
+	for(i=1;i<7;i++)
+		b[i]=0xa0+i;
+	printf("%c%c", a, b[3]);
+	printf("%c%c", a, b[4]);
+	printf("\n");
+	printf("%c%c", a, b[6]);
+	printf("%c%c", a, b[5]);
+	printf("\n");
+}
+
 //길이가 n인 표준 정사각형
 #include <stdio.h>
 void draw_square(int size);
